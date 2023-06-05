d1922738fa4e77be32cf858d3dfcbc908b6771a9
Florian Schlembach <1262767+floschl@users.noreply.github.com>
2023-06-05 19:55:43 +0200

    add back pysamosa/version.py

--- pysamosa/__init__.py
+++ pysamosa/__init__.py
@@ -1,5 +1,6 @@
 """Top-level package for PySAMOSA."""
+from pysamosa.version import __version__
 
 __author__ = """Florian Schlembach"""
 __email__ = "florian.schlembach@tum.de"
-__version__ = "0.2.0"
+__version__ = __version__
---
+++ pysamosa/version.py
@@ -0,0 +1,2 @@
+# see http://semver.org/
+__version__ = "0.2.0"
