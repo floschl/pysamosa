860f75c5efcfeac3672a614db0ce1023f96c147c
Florian Schlembach <1262767+floschl@users.noreply.github.com>
2023-06-06 12:43:15 +0200

    Bump version: 0.2.1 → 0.2.2

--- pysamosa/version.py
+++ pysamosa/version.py
@@ -1,2 +1,2 @@
 # see http://semver.org/
-__version__ = "0.2.1"
+__version__ = "0.2.2"
--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     tests_require=test_requirements,
     ext_modules = extensions,
     url='https://github.com/floschl/pysamosa',
-    version='0.2.1',
+    version='0.2.2',
     zip_safe=False,
     setup_requires=[],
 )
