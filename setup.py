2faa931d6100a65710c35d21e888ef4b5a6221cb
Florian Schlembach <1262767+floschl@users.noreply.github.com>
2023-06-11 01:13:18 +0200

    Bump version: 0.2.5 → 0.2.6

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
     tests_require=test_requirements,
     ext_modules=extensions,
     url="https://github.com/floschl/pysamosa",
-    version="0.2.5",
+    version="0.2.6",
     zip_safe=False,
     setup_requires=[],
 )
