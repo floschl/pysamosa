cd9cc03d6eaf5e295dbb3f70df7648081fb068ff
Florian Schlembach <1262767+floschl@users.noreply.github.com>
2023-07-09 11:40:52 +0200

    Bump version: 0.4.0 → 0.4.1

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ setup(
     tests_require=test_requirements,
     ext_modules=extensions,
     url="https://github.com/floschl/pysamosa",
-    version="0.4.0",
+    version="0.4.1",
     zip_safe=False,
     setup_requires=[],
 )
